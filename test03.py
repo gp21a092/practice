f1016cde5b92cc1a87644674436a2b84c00c8bce
Ayumi <tanakaauo@gmail.com>
2023-12-22 17:30:03 +0900

    test

---
+++ test03.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# 乱数を生成
+x = np.random.rand(100)
+y = np.random.rand(100)
+ 
+# 散布図を描画
+plt.scatter(x, y)
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.show()
